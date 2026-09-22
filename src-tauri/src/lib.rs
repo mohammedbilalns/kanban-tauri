3285b5758c34a8a0c60f06f0274ddafb9152b939
mohammedbilalns <70318383+mohammedbilalns@users.noreply.github.com>
2026-09-22 15:28:36 +0530

    configured sqlite

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,8 +13,8 @@ fn say_age(age: i32,name : &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .invoke_handler(tauri::generate_handler![say_age])
+        .plugin(tauri_plugin_sql::Builder::default().build())
+        .invoke_handler(tauri::generate_handler![greet, say_age])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
