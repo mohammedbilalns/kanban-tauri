e35934b295f9b3f248959e747802b2f613e7f1c5
mohammedbilalns <70318383+mohammedbilalns@users.noreply.github.com>
2026-09-23 16:37:43 +0530

    remember window position

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -14,6 +14,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_sql::Builder::default().build())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .invoke_handler(tauri::generate_handler![greet, say_age])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
